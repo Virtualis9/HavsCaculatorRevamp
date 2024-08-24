04c990af94a447b15dd2a47edab45ea135386cc1
Christopher Peacock <122057441+Virtualis9@users.noreply.github.com>
2024-08-24 17:40:26 +0100

    corrected CSS for new features. got calculator fully functioning

---
+++ Script.js
@@ -0,0 +1,44 @@
+let history = []; 
+const form = document.querySelector('.form');
+const emailError = document.getElementById('emailError');
+const trap = document.getElementById('pot');
+
+function updateHistory (total) {
+    history.push(Number(total))
+    let result = 0
+    for(let i = 0; i < history.length; i++){
+        result +=history[i]
+    }
+    document.getElementById("total").value = result;
+}
+
+function calculate (inputElementByID1, inputElementByID2){
+    let vibration = document.getElementById(inputElementByID1).value
+    let mins = document.getElementById(inputElementByID2).value
+    const points = (vibration * vibration) * 2
+    const time = mins/60
+    const total = Math.round(points * time)
+  
+    updateHistory(total)
+
+}
+
+// Action Limit Value (ALV) 
+function ALV (ALVInput, ALVOutput) {
+    
+    const vibration = document.getElementById('ALVInput').value
+    const points = (vibration * vibration) * 2
+ 
+    const limit = 100 / points;
+  
+    const time = Math.floor(limit * 60)
+ 
+    document.getElementById(ALVOutput).value = `You can use this tool for ${time} minuites`
+    
+    return;
+
+
+
+
+
+}
