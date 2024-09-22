cbc39bc6402f55b4bde94a91fe958ee14236c0b7
virtualis9 <peacockchristopher15@gmail.com>
2024-09-22 12:48:46 +0100

    added forms that sends data to my google sheets! got a return button working to go back to main page

--- sighnUpScript.js
+++ sighnUpScript.js
@@ -3,3 +3,20 @@ function goBack(){
     window.location.href = 'index.html';
     return
 }
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycbxzTxukCi13mM5irfmfiRuv2zWZsjmWQVO7AqWvvXmwrXdZ0JxTUvYhMLKGUSOWyFV1Aw/exec';
+const form = document.getElementById('form');
+
+form.addEventListener('submit', e => {
+    window.location.href = 'index.html'
+    e.preventDefault();
+    fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok.');
+            } 
+        })
+        .catch(error => console.log('Error!', error.message));
+});
+
+
